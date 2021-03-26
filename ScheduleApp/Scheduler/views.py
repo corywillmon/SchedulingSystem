13c1007ef4dc77937b31f933feb55ea404972f6d
Cory Willmon <corywillmon123@gmail.com>
2021-03-26 10:46:44 -0500

    db connection

--- ScheduleApp/Scheduler/views.py
+++ ScheduleApp/Scheduler/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import sqlite3
+import os
 
 # Create your views here.
 
@@ -11,4 +13,20 @@ def login(request):
 #This is the transition to the StandardEmployeeHomepage
 #The user will reach this page after the login page if logged in successfully.
 def loginAction(request):
+    conn = sqlite3.connect('Schedule.db')
+
+    #checks connection in the database
+    print(chk(conn))
+
+    conn.close()
     return render(request, 'StandardEmployeeHomepage.html')
+
+
+#This function checks if the database connection is opened or closed.
+# will retur true for open and false for closed.
+def checkConnection(conn):
+    try:
+        conn.cursor()
+        return True 
+    except Exception as ex:
+        return False
